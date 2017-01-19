7c0180dd8a4314968754a9b43048050ff81c744e
sina6991 <sina_6991@Yahoo.com>
2017-01-19 17:54:23 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,6 +1,6 @@
 <?php
 define('BOT_TOKEN', '**TOKEN**');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('API_URL', 'https://api.telegram.org/bot'.268918464:AAHobYDUa0GCbjjRL3MvzRte6Sg-AobxwWY.'/');
 
 function apiRequestWebhook($method, $parameters) {
   if (!is_string($method)) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = **114092691**;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
